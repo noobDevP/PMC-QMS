f0d92f356a7fb4f4c32dc0a17755e4387714c6cb
Emmanuel Garcia <emmanuel.m.garcia@isu.edu.ph>
2026-08-17 16:04:02 +0800

    Allow Werkzeug server to run in production without eventlet

--- backend/app.py
+++ backend/app.py
@@ -48,4 +48,4 @@ def serve_uploads(filename):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
